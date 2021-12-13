7fa08a07be03be569d685e1145c2f629a1089eb0
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2021-12-13 10:19:03 +0100

    costructor and AddFollowedUser

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -6,27 +6,33 @@ namespace Collections
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
+        Dictionary<string,TUser> followed = Dictionary<string,TUser>();
+        
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
+
+            
+            //throw new NotImplementedException("TODO is there anything to do here?");
+
         }
 
-        public bool AddFollowedUser(string group, TUser user)
-        {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
-        }
+
+        public bool AddFollowedUser(string group,TUser user) => followed.TryAdd(group,user);
 
         public IList<TUser> FollowedUsers
         {
+            
             get
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                List<TUser> followers = new List<TUser>(followed.Values);
+                //throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
             }
         }
 
         public ICollection<TUser> GetFollowedUsersInGroup(string group)
         {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
+            return new CollectionExtensions = followed.key;
+            //throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
         }
     }
 }
